d11c3abef23ccb5bc6c3acd3be4007ab42ad865d
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-02-25 15:01:08 +0100

    create todo state variable

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
+import { useState } from 'react';
 import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-		  <h1>Todo List</h1>
-		  <div className="todoListArea">
-			<input type="text" /> <button>Add Todo</button>
-		  </div>
-    </div>
-  );
+	const [todo, setTodo] = useState('');
+	// second state variable...
+
+	return (
+		<div className="App">
+			<h1>Todo List ({todo})</h1>
+			<div className="todoListArea">
+				<input type="text" onChange={(e) => setTodo(e.target.value)} value={todo} /> <button>Add Todo</button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
