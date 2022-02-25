2631866e6ba2332e49fda01ae3e8dc85bd785740
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-02-25 15:06:38 +0100

    showing todos with dummy variables

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import './App.scss';
 
 function App() {
 	const [todo, setTodo] = useState('');
-	// second state variable...
+	const [todos, setTodos] = useState(['get milk', 'do report']);
 
 	return (
 		<div className="App">
 			<h1>Todo List ({todo})</h1>
 			<div className="todoListArea">
 				<input type="text" onChange={(e) => setTodo(e.target.value)} value={todo} /> <button>Add Todo</button>
+				<hr />
+				<ul>
+					{todos.map((todo, i) => {
+						return (
+							<li key={i}>{todo}</li>
+						)
+					})}
+				</ul>
 			</div>
 		</div>
 	);
