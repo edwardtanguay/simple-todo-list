98767a46dffd598394e5b5eb844ee7607fcc06c1
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-02-25 14:53:58 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+		  <h1>Todo List</h1>
+		  <div className="todoListArea">
+			<input type="text" /> <button>Add Todo</button>
+		  </div>
+    </div>
+  );
+}
+
+export default App;
